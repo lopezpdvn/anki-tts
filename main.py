3174edf4cfbecfcb99556faf2bfea79edbbb4959
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-09-21 00:26:13 -0700

    WIP

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ tree = ET.parse('example_tmpl.xml')
 root = tree.getroot()
 
 p = ET.SubElement(root, 'p')
-#s = ET.SubElement(p, <s>To encourage, support, or cheer for someone or something; to wish the best for someone or something in an endeavor or activity.</s>
+s = ET.SubElement(p, 's')
+s.text = 'To encourage, support, or cheer for someone or something; to wish the best for someone or something in an endeavor or activity.'
+
+brk = ET.SubElement(p, 'break', strength='none', time='4s')
+
+s = ET.SubElement(p, 's')
+s.text = 'root for'
 
 ET.dump(root)
