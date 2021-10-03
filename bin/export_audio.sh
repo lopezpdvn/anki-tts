2df5f251a8abca67c4ee507a4dadc39f2225d8f9
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-10-03 15:05:28 -0700

    update script

--- bin/export_audio.sh
+++ bin/export_audio.sh
@@ -3,4 +3,4 @@ set -x #echo on
 
 python3 main.py > example.xml
 espeak -m -f example.xml -s 150 -w example.wav
-ffmpeg -i example.wav -ab 256k "/Users/pedrolopez/Google Drive/My Drive/phrasal_verbs.mp3"
+ffmpeg -y -i example.wav -ab 256k "/Users/pedrolopez/Google Drive/My Drive/proj/anki-tts/phrasal_verbs.mp3"
