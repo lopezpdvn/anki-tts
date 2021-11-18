2bad3208b94ffbdf3105e175bb91f08e7d091170
Pedro Ivan Lopez <m@pedroivanlopez.com>
2021-11-17 20:47:53 -0800

    improve audio conversion command

--- bin/export_audio.sh
+++ bin/export_audio.sh
@@ -3,4 +3,4 @@ set -x #echo on
 
 python3 main.py > example.xml
 espeak -m -f example.xml -s 150 -w example.wav
-ffmpeg -y -i example.wav -ab 256k "/Users/pedrolopez/Google Drive/My Drive/proj/anki-tts/phrasal_verbs.mp3"
+/usr/local/bin/ffmpeg -y -i example.wav -ab 256k "/Users/pedrolopez/Google Drive/My Drive/proj/anki-tts/phrasal_verbs.mp3"
