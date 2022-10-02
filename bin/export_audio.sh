0378ec71824c8c65d86350077543e50a4846cafe
Pedro Ivan Lopez <m@pedroivanlopez.com>
2022-10-02 12:40:30 -0700

    change speed and voice

--- bin/export_audio.sh
+++ bin/export_audio.sh
@@ -2,6 +2,6 @@
 set -x #echo on
 
 python3 main.py > example.xml
-espeak -m -f example.xml -s 150 -w example.wav
+espeak -m -f example.xml -w example.wav -v "us-mbrola-1"
 rm -f "/Users/pedrolopez/Google Drive/My Drive/proj/anki-tts/phrasal_verbs.mp3"
 /usr/local/bin/ffmpeg -y -i example.wav -ab 256k "/Users/pedrolopez/Google Drive/My Drive/proj/anki-tts/phrasal_verbs.mp3"
