e8f71bdd31bef4fd120b9ad88234e7cb27a3bdf2
Pedro Lopez Hernandez <pedrolopez@meta.com>
2023-11-28 20:33:23 -0800

    speed up

--- bin/export_audio.sh
+++ bin/export_audio.sh
@@ -2,6 +2,6 @@
 set -x #echo on
 
 python3 main.py > example.xml
-espeak -m -f example.xml -w example.wav -v "us-mbrola-1"
+espeak -m -f example.xml -w example.wav -v "us-mbrola-1" -s 250
 rm -f "/Users/pedrolopez/Google Drive/My Drive/proj/anki-tts/phrasal_verbs.mp3"
 /usr/local/bin/ffmpeg -y -i example.wav -ab 256k "/Users/pedrolopez/Library/CloudStorage/GoogleDrive-pedrolopezhernandezfb@gmail.com/My Drive/proj/anki-tts/phrasal_verbs.mp3"
